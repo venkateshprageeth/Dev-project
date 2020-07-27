6d8a58829fe1f2c926cf3ba99a0157b9db1d724d
root <root@puppet-master.venkat.com>
2020-07-27 08:41:36 +0000

    Adding sleep to right resource

--- root-dev_proj/manifests/exec.pp
+++ root-dev_proj/manifests/exec.pp
@@ -4,15 +4,14 @@ command => '/bin/docker rm -f $(sudo docker ps -a -q)',
 #before => Exec['build'],
 #unless => '/bin/docker ps | grep website 1>/dev/null',
 #notify => Exec['build'],
-#onlyif => '/bin/docker ps | grep website'
-tries => 2,
-try_sleep => 10,
 onlyif => '/bin/docker ps | grep website'
 }
 exec { 'build':
 command => '/bin/docker build . -t website',
 #require => Exec['rm'],
 #onlyif => '/bin/docker ps | grep website'
+tries => 2,
+try_sleep => 10,
 }
 exec { 'run':
 command => '/bin/docker run -it -d -p 82:80 -d website',
